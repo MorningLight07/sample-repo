248137c0f4096605ca9c17336719c376f81257aa
Alec <joshuayumague07@gmail.com>
2024-03-27 20:34:11 +0800

    i dont know what to do

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from selenium. webdriver.chrome.service import Service
 from selenium. webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-<<<<<<< HEAD
 options = Options()
 options.add_experimental_option("detach", True)
 
@@ -11,10 +10,5 @@ driver = webdriver.Chrome(service=Service (ChromeDriverManager().install()),
                           options=options)
                                                                        
 driver.get("http://127.0.0.1:8000/createposts")
-=======
-driver = webdriver.Chrome()
->>>>>>> 6b34438a7cab788745049dafd2732326dd262563
 
-
-
-baho kag igit
+nalilibog na ako
