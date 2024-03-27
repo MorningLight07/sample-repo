2c8945a6077867d8fcb732ad6f3d857fa49c198c
Alec <joshuayumague07@gmail.com>
2024-03-27 19:37:38 +0800

    commit message all changes are for weak being never to change just being real

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
+from selenium. webdriver.chrome.service import Service
+from selenium. webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(executable_path = 'sample-repo\chromedriver-win64')
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service (ChromeDriverManager().install()),
+                          options=options)
+                                                                       
+driver.get("http://127.0.0.1:8000/createposts")
